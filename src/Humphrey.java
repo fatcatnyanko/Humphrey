a58ca5ba2c0a2356e92d52888c6f561d2464b118
Grace Ho <Grace Ho@LAPTOP-QR1O484K.lan>
2025-04-14 01:22:20 -0700

    please

---
+++ src/Humphrey.java
@@ -0,0 +1,33 @@
+/**
+* Lead Author(s):
+* @author Grace Ho; student ID
+* @author Full name; student ID
+* <<Add additional lead authors here>>
+*
+* Other Contributors:
+* Full name; student ID or contact information if not in class
+* <<Add additional contributors (mentors, tutors, friends) here, with contact information>>
+*
+* References:
+* Morelli, R., & Walde, R. (2016).
+* Java, Java, Java: Object-Oriented Problem Solving
+* https://open.umn.edu/opentextbooks/textbooks/java-java-java-object-oriented-problem-solving
+*
+* <<Add more references here>>
+*
+* Version: 2025-04-14
+*/
+
+/**
+ * Purpose: The reponsibility of Humphrey is ...
+ *
+ * Humphrey is-a ...
+ * Humphrey is ...
+ */
+public class Humphrey
+{
+	public Humphrey()
+	{
+		System.out.print("humphrey");
+	}
+}
